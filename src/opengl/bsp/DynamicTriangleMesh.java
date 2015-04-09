bd8c30445960f729f4c0ba0e73e02a7285e7f8fb
Jan-Philipp Kappmeier <jp.kappmeier@gmail.com>
2015-04-09 23:59:27 +0200

    OpenGL tools library init.
    Takeover of legacy code.

---
+++ src/opengl/bsp/DynamicTriangleMesh.java
@@ -0,0 +1,88 @@
+/* zet evacuation tool copyright (c) 2007-14 zet evacuation team
+ *
+ * This program is free software; you can redistribute it and/or
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package opengl.bsp;
+
+import de.tu_berlin.math.coga.math.vectormath.Vector3;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * A mesh consisting of triangles. The mesh faces are stored in a queue. The
+ * vertices can be fast accessed via indices, they are stored in an
+ * {@link java.util.ArrayList}.
+ * @author Jan-Philipp Kappmeier
+ */
+public class DynamicTriangleMesh extends Mesh<Triangle> {
+	Queue<Triangle> facesQueue;
+	ArrayList<Vector3> verticesArray;
+
+	public DynamicTriangleMesh() {
+		verticesArray = new ArrayList<Vector3>();
+		facesQueue = new LinkedList<Triangle>();
+		faces = facesQueue;
+		vertices = verticesArray;
+	}
+
+	/**
+	 * Creates a triangle consisting of the specified vertices.
+	 * @param i1 the index of the first vertex
+	 * @param i2 the index of the second vertex
+	 * @param i3 the index of the third vertex
+	 * @return
+	 */
+	public Triangle addTriangle( int i1, int i2, int i3 ) {
+		final Triangle t = new Triangle( verticesArray.get( i1 ), verticesArray.get( i2 ), verticesArray.get( i3 ), i1, i2, i3 );
+		faces.add( t );
+		return t;
+	}
+
+	public Triangle addTriangle2( int i1, int i2, int i3 ) {
+		final Triangle t = new Triangle( verticesArray.get( i1 ), verticesArray.get( i2 ), verticesArray.get( i3 ), i1, i2, i3 );
+		//faces.add( t );
+		return t;
+	}
+
+	public boolean offer( Triangle e ) {
+		return facesQueue.offer( e );
+	}
+
+	public Triangle remove() {
+		return facesQueue.remove();
+	}
+
+	public Triangle poll() {
+		return facesQueue.poll();
+	}
+
+	public Triangle element() {
+		return facesQueue.element();
+	}
+
+	public Triangle peek() {
+		return facesQueue.peek();
+	}
+
+	@Override
+	public void prepareSize( int vertices, int edges, int faces ) {
+		verticesArray.ensureCapacity( vertices );
+	}
+
+	Queue<Triangle> getQueue() {
+		return facesQueue;
+	}
+}
---
+++ src/opengl/bsp/Triangle.java
@@ -0,0 +1,64 @@
+/* zet evacuation tool copyright (c) 2007-14 zet evacuation team
+ *
+ * This program is free software; you can redistribute it and/or
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package opengl.bsp;
+
+import de.tu_berlin.math.coga.math.vectormath.Plane;
+import de.tu_berlin.math.coga.math.vectormath.Vector3;
+
+/**
+ *
+ * @author Jan-Philipp Kappmeier
+ */
+public class Triangle implements Face {
+	Vector3 v[] = new Vector3[3];
+	int vidx[] = new int[3];
+	Plane plane;
+	Vector3 faceNormal;
+
+	public Triangle() {
+		// gefährlich ;)
+	}
+
+	public Triangle( Vector3 v0, Vector3 v1, Vector3 v2, int v0idx, int v1idx, int v2idx ) {
+		v[0] = v0;
+		v[1] = v1;
+		v[2] = v2;
+		vidx[0] = v0idx;
+		vidx[1] = v1idx;
+		vidx[2] = v2idx;
+		computePlane();
+	}
+
+	/**
+	 * Recomputes the plane equations and the normal. This may be called in the
+	 * case, the vertex coordinates have been changed. Additionally, this should
+	 * be called in case the normal and plane haven't been computed yet.
+	 */
+	public void computePlane() {
+		if( plane == null )
+			plane = new Plane( v[0], v[1], v[2] );
+		else
+			plane.setPlane( v[0], v[1], v[2] );
+		//BspMain.planes.add( plane )
+		faceNormal = plane.getNormal();
+	}
+
+	@Override
+	public String toString() {
+		return v[0].toString() + " " + v[1].toString() + " " + v[2].toString();
+	}
+}
